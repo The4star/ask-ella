632f636cbb2cf5fc314b404a2111875a9ad1e3b1
Clinton Forster <clintonforster@gmail.com>
2020-08-05 18:06:13 +1000

    finished desktop and work and services page

--- src/components/services/services.component.jsx
+++ src/components/services/services.component.jsx
@@ -1,11 +1,46 @@
 import React from 'react'; 
 
-const Services = () => {
+import InformationPopup from '../information-popup/information-popup.component'
+
+const togglePopup = (id) => {
+  const popupToToggle = document.querySelector(`#${id}`)
+  
+  if (popupToToggle.style.display === "none") {
+    return popupToToggle.style.display = "flex"
+  } else {
+    return popupToToggle.style.display = "none"
+  }
+}
+
+const Services = ({content}) => {
 
   return (
     <div className="services-section">
-      <div className="content-wrapper">
-        
+      <div className="services-content-wrapper">
+        <div className="header">
+          <h2>
+            Services
+          </h2>
+        </div>
+        <div className="paragraph-content" dangerouslySetInnerHTML={{ __html: content[0].contentText }} >
+        </div>
+        <div className="service-list">
+          {
+            content.map(service => {
+              if (service.serviceContent) {
+                return (
+                  <div key={`${service.title}`}>
+                    <button  className="service-title" onClick={() => togglePopup(`${service.title}-popup`)}>
+                      {service.title}
+                    </button>
+                    <InformationPopup id={`${service.title}-popup`} colour="red" title={service.title} content={service.serviceContent} togglePopup={togglePopup} /> 
+                  </div>
+                )
+              }
+              return null
+            })
+          }
+        </div>
       </div>
     </div>
   )
--- src/components/work/work.component.jsx
+++ src/components/work/work.component.jsx
@@ -1,15 +1,64 @@
 import React from 'react'; 
 
-const Work = () => {
+import InformationPopup from '../information-popup/information-popup.component'
 
+const togglePopup = (id) => {
+  const popupToToggle = document.querySelector(`#${id}`)
+  
+  if (popupToToggle.style.display === "none") {
+    return popupToToggle.style.display = "flex"
+  } else {
+    return popupToToggle.style.display = "none"
+  }
+}
+
+const Work = ({content}) => {
+console.log(content)
   return (
     <div className="work-section">
-      <div className="content-wrapper">
+      <div className="work-content-wrapper">
         <div className="header">
           <h2>
-            Work
+            Work & fun fac<span>t</span>s
           </h2>
         </div>
+        <div className="list-area">
+          <div className="work-list">
+            {
+              content.map(section => {
+                if (section.workContent) {
+                  return (
+                    <div key={`${section.title}`}>
+                      <button  className="work-title" onClick={() => togglePopup(`${section.title.replace(/ /g, "-")}-popup`)}>
+                        {section.title}
+                      </button>
+                      <InformationPopup id={`${section.title.replace(/ /g, "-")}-popup`} colour="blue" title={section.title} content={section.workContent} togglePopup={togglePopup} /> 
+                    </div>
+                  )
+                }
+                return null
+              })
+            }
+          </div>
+          <div className="fun-list">
+            {
+              content.map(section => {
+                if (section.funFactContent) {
+                  return (
+                    <div key={`${section.title}`}>
+                      <button  className="fun-title" onClick={() => togglePopup(`${section.title.replace(/ /g, "-")}-popup`)}>
+                        {section.title}
+                      </button>
+                      <InformationPopup id={`${section.title.replace(/ /g, "-")}-popup`} colour="blue" title={section.title} content={section.funFactContent} togglePopup={togglePopup} funHeader/> 
+                    </div>
+                  )
+                }
+                return null
+              })
+            }
+          </div>
+        </div>
+        
       </div>
     </div>
   )

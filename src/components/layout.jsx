45a70b26989564c8a1196ee042bb0be5d1dcbc52
Clinton Forster <clintonforster@gmail.com>
2020-08-12 12:19:58 +1000

    add site credits

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -10,11 +10,22 @@ import PropTypes from "prop-types"
 // import { useStaticQuery, graphql } from "gatsby"
 
 // local images
-import { linkedIn, imdb } from '../images/image-variables'
+import { linkedIn, imdb, closeIcon } from '../images/image-variables'
 
 // icons 
 import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
 import EmailIcon from '@material-ui/icons/Email';
+
+const handleCredits = () => {
+  const credits = document.querySelector(".credits")
+
+  if (credits.style.display === "block") {
+    credits.style.display = "none";
+  } else {
+    credits.style.display = "block";
+  }
+}
+
 const Layout = ({ children }) => {
 //   const data = useStaticQuery(graphql`
 //     query SiteTitleQuery {
@@ -38,6 +49,22 @@ const Layout = ({ children }) => {
           <p><PhoneIphoneIcon className="phone-icon" /> 0403138275</p>
           <a href="https://www.linkedin.com/in/ellalehaf/" target="_blank" rel="noopener noreferrer">{linkedIn}</a>
           <a href="https://www.imdb.com/name/nm2858095/" target="_blank" rel="noopener noreferrer" id="imdb">{imdb}</a>
+          <button onClick={handleCredits} className="credits-button">Site Credits</button>
+          <div className="credits" style={{display: "none"}}>
+          <button className={`close-button`} onClick={() => handleCredits()} >{closeIcon}</button>
+            <h4>
+              Illustrations
+            </h4>
+            <a href= "http://max-dalton.com/" target="_blank" rel="noopener noreferrer">
+              Max Dalton Illustration
+            </a>
+            <h4>
+              Site Design and Development
+            </h4>
+            <a href="https://the-4star-045711.netlify.app/" target="_blank" rel="noopener noreferrer">
+              Clinton Forster
+            </a>
+          </div>
         </footer>
       </div>
     </>
